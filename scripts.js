deecd748573f3b6683436361f9413888f73ab34c
rs <skasal@mail.ru>
2023-07-12 18:11:11 +0600

    version 1.0 ready

--- scripts.js
+++ scripts.js
@@ -7,3 +7,33 @@ window.onscroll = () => {
     header.classList.remove('coloured');
   }
 };
+
+
+// Scrolling
+function scrollToEducation(event) {
+  event.preventDefault();
+
+  var targetElem = document.getElementById('services');
+  targetElem.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
+function scrollToMission(event) {
+  event.preventDefault();
+
+  var targetElem = document.getElementById('mission');
+  targetElem.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
+function scrollToMentors(event) {
+  event.preventDefault();
+
+  var targetElem = document.getElementById('mentors');
+  targetElem.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
+function scrollToLearn(event) {
+  event.preventDefault();
+
+  var targetElem = document.getElementById('choose-role-buttons');
+  targetElem.scrollIntoView({block: "center", behavior: "smooth"});
+}
